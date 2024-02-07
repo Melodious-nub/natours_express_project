45edcdf51969a6e8e262146b35590a939e1c983d
Melodious-nub <shawon.taluckder1@gmail.com>
2024-02-07 17:32:33 +0600

    updated with mongoose connected wih my app

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // environment should be import before the app module
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./index');
-const port = process.env.port;
 
-// console.log(process.env);
+dotenv.config({ path: './config.env' });
+const port = process.env.port;
+const DB = process.env.DATABASE;
+
+// console.log(process.env.DATABASE);
+// in one line callback
+mongoose.connect(DB).then(() => console.log('Database is connected'));
 
 app.listen(port, () => {
     console.log('App running on port ' + '127.0.0.1:' + port);
